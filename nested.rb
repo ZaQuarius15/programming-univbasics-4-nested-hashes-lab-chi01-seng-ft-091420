0f0b78221547e7f9a1ed8e937943f11e3b5f61c6
Zack Carlson <jzcarlson014@gmail.com>
2020-08-21 17:44:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,6 +16,8 @@ def hopper
       }
     }
 
+programmer_hash[:grace_hopper]
+
 end
 
 def alan_kay_is_known_for
@@ -37,6 +39,8 @@ def alan_kay_is_known_for
       }
     }
 
+programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+
+programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
